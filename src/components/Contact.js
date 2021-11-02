a90799ffd34f4e57d25b74afb2d79f8dec68c07b
winstonwijaya <winstonwijaya59@gmail.com>
2021-11-02 16:01:40 +0700

    update css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,15 +4,15 @@ import { contactInfo } from '../portofolio'
 
 export default function Contact() {
     return (
-        <div className="container d-flex my-5">
-            <div className="main contact-margin-top flex-fill" id="contact">
+        <div className="container d-flex my-5"  id="contact">
+            <div className="main contact-margin-top flex-fill">
                 <div className="contact-div-main">
                     <div className="contact-header">
-                        <h1 className="contact-heading contact-title">{contactInfo.title}</h1>
-                        <p className= "subTitle contact-subtitle bold">
+                        <h1 className="contact-heading contact-title lh-lg">{contactInfo.title}</h1>
+                        <p className= "subTitle contact-subtitle bold lh-lg">
                             {contactInfo.subtitle}
                         </p>
-                        <div className= "contact-text-div">
+                        <div className= "contact-text-div mb-5">
                             <a
                                 className="contact-detail-email"
                                 href={"mailto:" + contactInfo.email_address}
@@ -20,10 +20,10 @@ export default function Contact() {
                                 {contactInfo.email_address}
                             </a>=
                             <SocialMedia />
-                            <h6 className="dark py-3">
+                            <p className="dark py-3 lh-lg">
                                 Made by Winston Wijaya<br/>
                                 Theme by <a href="https://github.com/saadpasta/developerFolio">developerFolio</a>
-                            </h6>
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-import {Link} from 'react-scroll'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
+
     return (
         <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
             <div className="container-fluid nav-container">
